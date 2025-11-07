7b48078886f41bcab044b88caf7ff0578110ec7f
lihaoyu <lihaoyu@zj.tech>
2025-11-07 15:29:29 +0800

    test

--- src/main/java/com/lhy/okhttp/controller/UserController.java
+++ src/main/java/com/lhy/okhttp/controller/UserController.java
@@ -9,6 +9,7 @@ package com.lhy.okhttp.controller;
  * @since 2025/10/28 15:41
  */
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.lhy.okhttp.domain.User;
 import com.lhy.okhttp.domain.request.user.UserManageAddReq;
 import com.lhy.okhttp.service.UserService;
@@ -30,12 +31,14 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/test")
+    @DS("master")
     public String test() {
         return "Hello, this is a test!";
     }
 
     @GetMapping("/list")
     @Operation(summary =  "用户列表")
+    @DS("master")
     public List<User> userList() {
         List<User> list = userService.lambdaQuery().list();
         return list;
@@ -43,6 +46,7 @@ public class UserController {
 
     @PostMapping("/add")
     @Operation(summary = "添加用户")
+    @DS("master")
     public String userAdd(@RequestBody UserManageAddReq req){
         User user = new User();
         user.setName(req.getPassword());
